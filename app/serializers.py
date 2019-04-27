1bd94511c7db33945c3bf78598c5173afbf61039
seba608 <seortiz@uc.cl>
2019-04-27 15:17:25 -0400

    Serializers

--- app/serializers.py
+++ app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from app.models import Order
+from app.models import Order, Product, Ingredient, Assigment
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -7,3 +7,26 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ('id', 'created', 'client_group', 'sku', 'amount',
                   'storeId', 'accepted', 'dispatched')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'sku', 'name', 'description', 'sale_price', 'number_ingredients_needed',
+                  'number_products', 'expected_duration_time', 'equivalence_units',
+                  'measurement_unit', 'production_lot', 'expected_production_time',
+                  'producer_groups', 'dispatch_space', 'reception_space', 'minimum_stock')
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'product_sku', 'product_name', 'ingredient_sku', 'ingredient_name',
+                  'quantity', 'measurement_unit', 'production_lot', 'quantity_for_lot',
+                  'units_quantity')
+
+
+class AssigmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assigment
+        fields = ('id', 'sku', 'group')
