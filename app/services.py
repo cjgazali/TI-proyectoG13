23173ae25a0ebfb3d6d52d08785d4d91b27cf570
seba608 <seortiz@uc.cl>
2019-05-20 23:11:23 -0400

    Servicios de OC y ordenes servidor

--- app/services.py
+++ app/services.py
@@ -1,15 +1,20 @@
+from datetime import datetime
 import requests
 import json
 import hashlib
+import xml.etree.ElementTree as ET
+import pysftp
+import dateutil.parser
 import hmac
 from base64 import encodestring
 
-context = "PRODUCTION"  # or DEVELOPMENT
+context = "DEVELOPMENT"  # or DEVELOPMENT
 # url API profe
 if context == "PRODUCTION":
     url_base = "https://integracion-2019-prod.herokuapp.com/bodega"
 else:
     url_base = "https://integracion-2019-dev.herokuapp.com/bodega"
+    url_oc = "https://integracion-2019-dev.herokuapp.com/oc"
 
 # url API grupos
 server_url = "http://tuerca{}.ing.puc.cl"
@@ -122,6 +127,89 @@ def post_order(n_group, sku, quantity, id_almacen_despacho):
     return response
 
 
+# Consulta una orden de compra
+def consultar_oc(id_orden):
+    url = url_oc + '/obtener/{}'.format((id_orden))
+    headers = {'Content-Type': 'application/json'}
+    body = {'id': id_orden}
+    result = requests.get(url, data=json.dumps(body), headers=headers)
+    response = json.loads(result.text)
+    return response
+
+
+# Crear una orden de compra
+def crear_oc(cliente, proveedor, sku, fecha_entrega, cantidad, precio_unitario, canal, url_notificacion):
+    url = url_oc + '/crear'
+    headers = {'Content-Type': 'application/json'}
+    body = {'cliente': cliente, 'proveedor': proveedor, 'sku': int(sku),
+            'fechaEntrega': int(fecha_entrega), 'cantidad': int(cantidad),
+            'precioUnitario': int(precio_unitario), 'canal': canal,
+            'urlNotificacion': url_notificacion}
+    result = requests.put(url, data=json.dumps(body), headers=headers)
+    response = json.loads(result.text)
+    return response
+
+
+# Anula una orden de compra
+def anular_oc(id_orden):
+    url = url_oc + '/anular/{}'.format(id_orden)
+    headers = {'Content-Type': 'application/json'}
+    body = {'id': id_orden, 'anulacion': 'Anulación de prueba'}
+    result = requests.delete(url, data=json.dumps(body), headers=headers)
+    response = json.loads(result.text)
+    return response
+
+
+# Rechazar oc
+def rechazar_oc(id_orden):
+    url = url_oc + '/rechazar/{}'.format(id_orden)
+    headers = {'Content-Type': 'application/json'}
+    body = {'id': id_orden, 'rechazo': 'Reechazo de prueba'}
+    result = requests.post(url, data=json.dumps(body), headers=headers)
+    response = json.loads(result.text)
+    return response
+
+
+# Recepcionar oc
+def recepcionar_oc(id_orden):
+    url = url_oc + '/recepcionar/{}'.format(id_orden)
+    headers = {'Content-Type': 'application/json'}
+    body = {'id': id_orden}
+    result = requests.post(url, data=json.dumps(body), headers=headers)
+    response = json.loads(result.text)
+    return response
+
+
+# Ver servidor
+def prueba_servidor():
+    host_name = "fierro.ing.puc.cl"
+    user_name = "grupo13_dev"
+    password = "c7vq41weKJGcvas"
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+    with pysftp.Connection(host=host_name, username=user_name, password=password, port=22, cnopts=cnopts) as sftp:
+        print("Connection succesfully stablished ... ")
+        sftp.cwd('/pedidos')
+        directory_structure = sftp.listdir_attr()
+        cont = 0
+        for attr in directory_structure:
+            cont += 1
+            if cont < 3:
+                with sftp.open(attr.filename) as archivo:
+                    tree = ET.parse(archivo)
+                    root = tree.getroot()
+                    for elem in root:
+                        if elem.tag == 'id':
+                            print('id a consultar: {}'.format(elem.text))
+                            response = consultar_oc(elem.text)
+                            fecha_entrega = response[0]['fechaEntrega']
+                            fecha_aux = dateutil.parser.parse(fecha_entrega)
+                            print(fecha_aux, type(fecha_aux))
+                            print(fecha_entrega)
+                            # Desarrollar algoritmo
+            print(attr.filename, type(attr.filename))
+
+
 if __name__ == '__main__':
     #a = obtener_almacenes()
     #for elem in a:
@@ -134,11 +222,20 @@ if __name__ == '__main__':
     #l = mover_entre_almacenes('5cc6250c93360b0004f0431b', '5cbd3ce444f67600049431fc')
     #print(l)
     # Este método es el que no está testeado aún, le pregunté al profe en una issue que onda
-    #f = mover_entre_bodegas('5cc22e96aa013f0004f0867e', '5cbd3ce444f67600049431d1')
+    #f = mover_entre_bodegas('5cc22ede compra96aa013f0004f0867e', '5cbd3ce444f67600049431d1')
     #print(f)
-    f = obtener_productos_almacen('5cbd3ce444f67600049431ff', '1001')
-    print(f)
-
-
+    #f = obtener_productos_almacen('5cbd3ce444f67600049431ff', '1001')
+    #print(f)
+    fecha_minima = "1970-01-01"
+    fecha1 = datetime.strptime(fecha_minima, '%Y-%m-%d')
+    tomorrow = "2019-05-22"
+    fecha2 = datetime.strptime(tomorrow, '%Y-%m-%d')
+    diff = fecha2 - fecha1
+    #print(type(diff), type(diff * 1000), diff)
+    print(crear_oc('5cbd31b7c445af0004739bef', '5cbd31b7c445af0004739beb', 1001, diff.total_seconds() * 1000,
+                   1, 1, 'b2b', "http://ejemplo.com/notificacion/{_id}"))
+    #por_borrar = '5ce213be9305c300043eda19'
+    #print(rechazar_oc('5ce33bc7bed9c00004d897fa'))
+    #prueba_servidor()
     #print(obtener_productos_almacen('5cbd3ce444f67600049431fc', '1001'))
     #print(fabricar_sin_pago("1001", 10))
